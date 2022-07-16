c4b40b7ea0230327112449759f1e90897f7bba3a
Kim MinJun <minjun326@Kims-MacBook-Pro.local>
2022-07-16 20:50:32 +0900

    api

--- src/main/java/community/mingle/app/src/domain/Member.java
+++ src/main/java/community/mingle/app/src/domain/Member.java
@@ -12,6 +12,8 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -36,6 +38,7 @@ public class Member {
     private String nickName;
     private String email; //regex 추가
     private String pwd; //regex 추가
+
 
     /** 학교게시판*/
     @OneToMany(mappedBy = "member")
