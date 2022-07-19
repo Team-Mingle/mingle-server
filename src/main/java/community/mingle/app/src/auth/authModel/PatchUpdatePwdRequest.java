9798681a58281ba0518344c75f3c78d3a4350487
tanggu01 <tanggu1221@gmail.com>
2022-07-20 03:13:29 +0900

    add JWT & Encryption to 1.8, 1.9, 1.10 API

--- src/main/java/community/mingle/app/src/auth/authModel/PatchUpdatePwdRequest.java
+++ src/main/java/community/mingle/app/src/auth/authModel/PatchUpdatePwdRequest.java
@@ -1,9 +1,12 @@
 package community.mingle.app.src.auth.authModel;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class PatchUpdatePwdRequest {
+//    private Long userIdx;
     private String email;
     private String pwd;
     private String rePwd;
