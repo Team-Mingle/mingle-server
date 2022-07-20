b6aef3dda5d663f7fbe84d581f6a14fcdb18c708
LeoJung <leojung0909@gmail.com>
2022-07-20 17:58:50 +0900

    template class for post api package

---
+++ src/main/java/community/mingle/app/src/post/PostRepository.java
@@ -0,0 +1,9 @@
+package community.mingle.app.src.post;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class PostRepository {
+}
