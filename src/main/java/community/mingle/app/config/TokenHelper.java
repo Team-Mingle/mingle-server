0e8a15c3f416e86d3949353e860559feb84205e1
tanggu01 <tanggu1221@gmail.com>
2022-08-23 16:45:56 +0900

    redis setting ING

--- src/main/java/community/mingle/app/config/TokenHelper.java
+++ src/main/java/community/mingle/app/config/TokenHelper.java
@@ -2,6 +2,7 @@ package community.mingle.app.config;
 
 
 import community.mingle.app.config.handler.JwtHandler;
+import community.mingle.app.utils.RedisService;
 import io.jsonwebtoken.Claims;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -14,6 +15,7 @@ import java.util.Optional;
 public class TokenHelper {
 
     private final JwtHandler jwtHandler;
+    private final RedisService redisService;
     private final String key;
     private final long maxAgeSeconds;
 
@@ -21,11 +23,20 @@ public class TokenHelper {
     private static final String ROLE_TYPES = "ROLE_TYPES";
     private static final String MEMBER_ID = "MEMBER_ID";
 
-    public String createToken(PrivateClaims privateClaims) {
+    public String createAccessToken(PrivateClaims privateClaims) {
         return jwtHandler.createToken(key,
                 Map.of(MEMBER_ID, privateClaims.getMemberId(), ROLE_TYPES, privateClaims.getRoleTypes()),
                 maxAgeSeconds);
     }
+
+
+    public String createRefreshToken(PrivateClaims privateClaims) {
+        String refreshToken = jwtHandler.createToken(key,
+                Map.of(MEMBER_ID, privateClaims.getMemberId(), ROLE_TYPES, privateClaims.getRoleTypes()),
+                maxAgeSeconds);
+        redisService.setValues(privateClaims.getMemberId(), refreshToken, maxAgeSeconds);
+    }
+
 
     public Optional<PrivateClaims> parse(String token) {
         return jwtHandler.parse(key, token).map(this::convert);
