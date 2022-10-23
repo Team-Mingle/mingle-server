6c5d99f220bda897676f21358a158525e5f4cd90
tanggu01 <tanggu1221@gmail.com>
2022-10-23 20:00:18 +0900

    5 ) /home/best/univ & /home/best/total 홈화면 베스트 게시물 API 추가

---
+++ src/main/java/community/mingle/app/src/home/HomeService.java
@@ -0,0 +1,51 @@
+package community.mingle.app.src.home;
+
+import community.mingle.app.config.BaseException;
+import community.mingle.app.src.domain.Member;
+import community.mingle.app.src.domain.Total.TotalPost;
+import community.mingle.app.src.domain.Univ.UnivPost;
+import community.mingle.app.src.post.PostRepository;
+import community.mingle.app.utils.JwtService;
+import community.mingle.app.utils.S3Service;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static community.mingle.app.config.BaseResponseStatus.DATABASE_ERROR;
+import static community.mingle.app.config.BaseResponseStatus.EMPTY_BEST_POSTS;
+
+@Service
+@RequiredArgsConstructor
+public class HomeService {
+    private final JwtService jwtService;
+    private final HomeRepository homeRepository;
+
+    /**
+     * 5.1 홈 전체 배스트 게시판 API
+     */
+    public List<TotalPost> findTotalPostWithMemberLikeComment() throws BaseException {
+        List<TotalPost> totalPosts = homeRepository.findTotalPostWithMemberLikeComment();
+        if (totalPosts.size() == 0) {
+            throw new BaseException(EMPTY_BEST_POSTS);
+        }
+        return totalPosts;
+    }
+
+    /**
+     * 5.2 홈 학교 베스트 게시판 API
+     */
+    public List<UnivPost> findAllWithMemberLikeCommentCount() throws BaseException {
+        Long memberIdByJwt = jwtService.getUserIdx();  // jwtService 의 메소드 안에서 throw 해줌 -> controller 로 넘어감
+        Member member;
+        member = homeRepository.findMemberbyId(memberIdByJwt);
+        if (member == null) {
+            throw new BaseException(DATABASE_ERROR); //무조건 찾아야하는데 못찾을경우 (이미 jwt 에서 검증이 되기때문)
+        }
+        List<UnivPost> univPosts = homeRepository.findAllWithMemberLikeCommentCount(member);
+        if (univPosts.size() == 0) {
+            throw new BaseException(EMPTY_BEST_POSTS);
+        }
+        return univPosts;
+    }
+}
---
+++ src/main/java/community/mingle/app/src/home/model/HomeBestTotalPostResponse.java
@@ -0,0 +1,33 @@
+package community.mingle.app.src.home.model;
+
+import community.mingle.app.src.domain.Total.TotalPost;
+import lombok.Getter;
+
+import static community.mingle.app.config.DateTimeConverter.convertLocaldatetimeToTime;
+
+@Getter
+public class HomeBestTotalPostResponse {
+    private Long postId;
+    private String title;
+    private String contents;
+    private String nickname;
+    private int likeCount;
+    private int commentCount;
+    private String createdAt;
+
+
+    public HomeBestTotalPostResponse(TotalPost totalPost) {
+        this.postId = totalPost.getId();
+        this.title = totalPost.getTitle();
+        this.contents = totalPost.getContent();
+        if (totalPost.getIsAnonymous() == true) {
+            this.nickname = "글쓴이";
+        } else {
+            this.nickname = totalPost.getMember().getNickname();
+        }
+        this.likeCount = totalPost.getTotalPostLikes().size();
+        this.commentCount = totalPost.getTotalPostComments().size();
+        this.createdAt = convertLocaldatetimeToTime(totalPost.getCreatedAt());
+    }
+
+}
