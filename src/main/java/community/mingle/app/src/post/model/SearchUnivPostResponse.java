867e14ba68ea39c8c8fe280242cee18568978d23
LeoJung <leojung0909@gmail.com>
2022-10-27 23:20:02 +0900

    add isFileAttached to home/best and post/search api

--- src/main/java/community/mingle/app/src/post/model/SearchUnivPostResponse.java
+++ src/main/java/community/mingle/app/src/post/model/SearchUnivPostResponse.java
@@ -1,31 +1,14 @@
 package community.mingle.app.src.post.model;
 
-import community.mingle.app.src.domain.Univ.UnivPost;
-import lombok.Getter;
+import java.util.List;
 
-import static community.mingle.app.config.DateTimeConverter.convertLocaldatetimeToTime;
-
-@Getter
 public class SearchUnivPostResponse {
 
-    private Long univPostId;
-    private String title;
-    private String content;
-    private String nickname;
-    private String createdAt;
+    private String boardName;
+    private List<SearchUnivPostDTO> postListDTO;
 
-
-
-    public SearchUnivPostResponse(UnivPost univPost) {
-        this.univPostId = univPost.getId();
-        this.title = univPost.getTitle();
-        this.content = univPost.getContent();
-        if (univPost.getIsAnonymous() == true) {
-            this.nickname = "글쓴이";
-        } else{
-            this.nickname = univPost.getMember().getNickname();
-        }
-        this.createdAt = convertLocaldatetimeToTime(univPost.getCreatedAt());
-
+    public SearchUnivPostResponse(String boardName, List<SearchUnivPostDTO> postListDTO) {
+        this.boardName = boardName;
+        this.postListDTO = postListDTO;
     }
 }
