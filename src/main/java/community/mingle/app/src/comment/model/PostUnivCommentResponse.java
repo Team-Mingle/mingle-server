cbcd296194c774c97152230695510f25f7a15169
tanggu01 <tanggu1221@gmail.com>
2022-11-09 21:34:45 +0900

    add createdAt for createComment API

--- src/main/java/community/mingle/app/src/comment/model/PostUnivCommentResponse.java
+++ src/main/java/community/mingle/app/src/comment/model/PostUnivCommentResponse.java
@@ -3,11 +3,14 @@ package community.mingle.app.src.comment.model;
 import community.mingle.app.src.domain.Univ.UnivComment;
 import lombok.Getter;
 
+import static community.mingle.app.config.DateTimeConverter.convertToDateAndTime;
+
 @Getter
 public class PostUnivCommentResponse {
 
     Long commentId;
     String nickname;
+    String createdAt;
 
     public PostUnivCommentResponse(Long anonymousId, UnivComment univComment) {
         this.commentId = univComment.getId();
@@ -16,6 +19,8 @@ public class PostUnivCommentResponse {
         } else{
             this.nickname = "익명 " + anonymousId;
         }
+        createdAt = convertToDateAndTime(univComment.getCreatedAt());
+
 
     }
 }
