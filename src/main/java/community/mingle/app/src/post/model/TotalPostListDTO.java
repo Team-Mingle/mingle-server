b595a023ea7659c7d666336dfed77d168c061686
tanggu01 <tanggu1221@gmail.com>
2022-12-17 18:49:24 +0900

    fix commentCount in PostList, BestPostList, SearchPostList DTO

--- src/main/java/community/mingle/app/src/post/model/TotalPostListDTO.java
+++ src/main/java/community/mingle/app/src/post/model/TotalPostListDTO.java
@@ -1,7 +1,12 @@
 package community.mingle.app.src.post.model;
 
+import community.mingle.app.src.domain.PostStatus;
 import community.mingle.app.src.domain.Total.TotalPost;
+import community.mingle.app.src.domain.Total.TotalComment;
 import lombok.Getter;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static community.mingle.app.config.DateTimeConverter.convertLocaldatetimeToTime;
 
@@ -29,7 +34,12 @@ public class TotalPostListDTO {
         }
         this.isFileAttached = totalPost.getIsFileAttached();
         this.likeCount = totalPost.getTotalPostLikes().size();
-        this.commentCount = totalPost.getTotalPostComments().size();
+//        this.commentCount = totalPost.getTotalPostComments().size();
+        /** 댓글 개수*/
+        List<TotalComment> commentList = totalPost.getTotalPostComments();
+        List<TotalComment> activeComments = commentList.stream().filter(ac -> ac.getStatus().equals(PostStatus.ACTIVE)).collect(Collectors.toList());
+        this.commentCount = activeComments.size();
+
         this.createdAt = convertLocaldatetimeToTime(totalPost.getCreatedAt());
 //        if(totalPost.getIsFileAttached() == true) {
 //            this.postImgUrl = totalPost.getTotalPostImages().get(0).getImgUrl(); //없는데 true여서 가져오려햇는데 어레이리스트 0번째가 없어서 인덱스에러남 이래서 디비에 막넣으면안됨 ;;
