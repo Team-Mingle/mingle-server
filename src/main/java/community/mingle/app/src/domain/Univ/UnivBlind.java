23f3a7d3ff3666741699c1e4f186916d7f5aa312
tanggu01 <tanggu1221@gmail.com>
2022-12-26 20:15:03 +0900

    Modify Blind Entity

--- src/main/java/community/mingle/app/src/domain/Univ/UnivBlind.java
+++ src/main/java/community/mingle/app/src/domain/Univ/UnivBlind.java
@@ -22,7 +22,7 @@ import java.util.Objects;
 public class UnivBlind {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "univblind_id")
+    @Column(name = "univ_blind_id")
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
