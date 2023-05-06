2f7b897fb891bb86dcc9ad2ba5acd419c39b03b4
Hyeonwoo <leojung0909@gmail.com>
2023-05-06 13:38:21 +0900

    feat: add content, location, imageList to 2.17 api

--- src/main/java/community/mingle/app/src/item/model/ItemListDTO.java
+++ src/main/java/community/mingle/app/src/item/model/ItemListDTO.java
@@ -1,11 +1,14 @@
 package community.mingle.app.src.item.model;
 
 import community.mingle.app.src.domain.Item;
+import community.mingle.app.src.domain.ItemImg;
 import community.mingle.app.src.domain.ItemLike;
 import community.mingle.app.src.domain.Total.TotalCommentLike;
 import lombok.Getter;
 
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 import static community.mingle.app.config.DateTimeConverter.convertLocaldatetimeToTime;
 
@@ -21,6 +24,9 @@ public class ItemListDTO {
     private int commentCount;
     private String status;
     private String imgThumbnailUrl;
+    private String content;
+    private String location;
+    private List<String> itemImgList;
 
     private boolean isLiked;
 
@@ -32,6 +38,13 @@ public class ItemListDTO {
         this.createdAt = convertLocaldatetimeToTime(item.getCreatedAt());
         this.likeCount = item.getItemLikeList().size();
         this.commentCount = item.getItemCommentList().size();
+        this.content = item.getContent();
+        this.location = item.getLocation();
+        if (item.getItemImgList().isEmpty()) {
+            this.itemImgList = null;
+        } else {
+            this.itemImgList = item.getItemImgList().stream().map(itemImg -> itemImg.getImgUrl()).collect(Collectors.toList());
+        }
         this.status = item.getStatus().getName();
         if (item.getItemImgList().isEmpty()) {
             this.imgThumbnailUrl = null;
