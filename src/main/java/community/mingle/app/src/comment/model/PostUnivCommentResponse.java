a2fc5924516749de6797733fec00c3d2f0759afd
Hyeonwoo <leojung0909@gmail.com>
2023-06-29 23:55:18 +0900

    feat: mypage 글, 댓글에 새내기 병아리 추가

--- src/main/java/community/mingle/app/src/comment/model/PostUnivCommentResponse.java
+++ src/main/java/community/mingle/app/src/comment/model/PostUnivCommentResponse.java
@@ -1,6 +1,7 @@
 package community.mingle.app.src.comment.model;
 
 import community.mingle.app.src.domain.Univ.UnivComment;
+import community.mingle.app.src.domain.UserRole;
 import lombok.Getter;
 
 import java.util.Objects;
@@ -10,33 +11,30 @@ import static community.mingle.app.config.DateTimeConverter.convertToDateAndTime
 @Getter
 public class PostUnivCommentResponse {
 
-    private Long commentId;
+    private final Long commentId;
     private String nickname;
-    private String createdAt;
-    private boolean isCommentFromAuthor;
-
+    private final String createdAt;
+    private final boolean isCommentFromAuthor;
 
 
     public PostUnivCommentResponse(Long anonymousId, UnivComment univComment, Long authorId) {
         Long commentWriter = univComment.getMember().getId();
 
         this.commentId = univComment.getId();
-        if (univComment.isAnonymous() == false && !(Objects.equals(commentWriter, authorId))) {
+        if (!univComment.isAnonymous() && !(Objects.equals(commentWriter, authorId))) {
             this.nickname = univComment.getMember().getNickname();
-        } else if (univComment.isAnonymous() && anonymousId != 0L){
+        } else if (univComment.isAnonymous() && anonymousId != 0L) {
             this.nickname = "익명 " + anonymousId;
         } else if (!univComment.isAnonymous() && Objects.equals(commentWriter, authorId)) {
             this.nickname = univComment.getMember().getNickname() + "(글쓴이)";
         } else if (univComment.isAnonymous() && Objects.equals(commentWriter, authorId)) {
             this.nickname = "익명(글쓴이)";
         }
-
-
-        if (Objects.equals(commentWriter, authorId)) {
-            isCommentFromAuthor = true;
-        } else {
-            isCommentFromAuthor = false;
+        if (univComment.getMember().getRole() == UserRole.FRESHMAN) {
+            this.nickname = "🐥" + this.nickname;
         }
+
+        isCommentFromAuthor = Objects.equals(commentWriter, authorId);
         createdAt = convertToDateAndTime(univComment.getCreatedAt());
 
 
