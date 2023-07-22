2af628040ad9057f8cb64d2eb9376eae2af2e449
Hyeonwoo <leojung0909@gmail.com>
2023-07-23 07:17:33 +0900

    fix: modify stream

--- src/main/java/community/mingle/app/src/item/ItemService.java
+++ src/main/java/community/mingle/app/src/item/ItemService.java
@@ -494,6 +494,6 @@ public class ItemService {
     public List<Currency> getCurrencyList(Long memberId) {
         Member member = memberRepository.findMember(memberId);
         Country country = member.getUniv().getCountry();
-        return Arrays.stream(Currency.values()).filter(it -> it.getCountires().contains(country)).collect(Collectors.toList());
+        return Arrays.stream(Currency.values()).filter(it -> it.getCountires().contains(country.getCountryName())).collect(Collectors.toList());
     }
 }
