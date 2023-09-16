52a147813a5010798022cf221a36a6986f47b4f8
Hyeonwoo <leojung0909@gmail.com>
2023-09-16 19:33:05 +0900

    feat: allow cors

--- src/main/java/community/mingle/app/AppApplication.java
+++ src/main/java/community/mingle/app/AppApplication.java
@@ -1,17 +1,19 @@
 package community.mingle.app;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 
+@OpenAPIDefinition(servers = {@Server(url = "https://dev.api.mingle.community/")})
 @SpringBootApplication
 public class AppApplication {
 
 
-	public static void main(String[] args) {
-		SpringApplication.run(AppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AppApplication.class, args);
+    }
 
 //	@PostConstruct
 //	public void started(){
